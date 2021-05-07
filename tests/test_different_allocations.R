07ef7363e203d1ea3f0b1c7e1dbafb7cff4816a1
osvaldoanacleto <junior242@gmail.com>
2021-05-07 15:33:50 +0200

    add initial test for random and 2FAM allocations

---
+++ tests/test_different_allocations.R
@@ -0,0 +1,24 @@
+install.packages("githubinstall")
+
+library(devtools)
+install_github("osvaldoanacleto/dnIGEmodel", ref = "experimental_design")
+library(dnIGEmodel)
+
+grp_size <- 10
+
+result_random <- generate_population(
+    allocation_type = "random",
+    num_replications =1,
+    group_size = grp_size,
+    sires = 25,
+    dpsire = 2
+  )
+
+result_2FAM <-
+  generate_population(
+    allocation_type = "2FAM",
+    num_replications = 1,
+    group_size = grp_size,
+    sires = 25,
+    dpsire = 2,
+  )
