860769664d4837654cfacef81333a312996de57b
Milena Lima <milenascimentolima@hotmail.com>
2021-05-08 17:55:44 -0300

    update

--- tests/test_different_allocations.R
+++ tests/test_different_allocations.R
@@ -1,5 +1,5 @@
 library(devtools)
-install_github("osvaldoanacleto/dnIGEmodel", ref = "experimental_design")
+#install_github("osvaldoanacleto/dnIGEmodel", ref = "experimental_design")
 library(dnIGEmodel)
 
 grp_size <- 10
@@ -8,8 +8,8 @@ result_random <- generate_population(
     allocation_type = "random",
     num_replications =1,
     group_size = grp_size,
-    sires = 25,
-    dpsire = 2
+    sires = 100,
+    dpsire = 20
   )
 
 result_2FAM <-
@@ -17,6 +17,10 @@ result_2FAM <-
     allocation_type = "2FAM",
     num_replications = 1,
     group_size = grp_size,
-    sires = 25,
-    dpsire = 2,
+    sires = 100,
+    dpsire = 20,
   )
+
+
+result_random
+result_2FAM
